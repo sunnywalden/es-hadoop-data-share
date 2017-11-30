d5c4c18efff68d82e196add4e86822ae0143d732
SUNNYWALDEN\Administrator <sunnywladen@gmail.com>
2017-11-30 18:53:53 +0800

    alter EH output to json without MR map key

--- src/main/java/es/hadoop/EH.java
+++ src/main/java/es/hadoop/EH.java
@@ -1,83 +1,94 @@
 package es.hadoop;
 
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
 import org.elasticsearch.hadoop.mr.EsInputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.hadoop.compression.lzo.DistributedLzoIndexer;
-
+import java.io.IOException;
 
 
 public class EH {
+    private static Logger LOG = LoggerFactory.getLogger(EH.class);
 
-    public static class EhMap extends Mapper<Writable, Writable, Text, Text> {
+    public static class EhMap extends Mapper<Writable, Writable, NullWritable, Text> {
         @Override
-        public void map(Writable key, Writable value, Mapper<Writable, Writable, Text, Text>.Context context)
+        public void map(Writable key , Writable value , Mapper<Writable, Writable, NullWritable, Text>.Context context)
                 throws IOException, InterruptedException {
-            context.write(new Text(key.toString()), new Text(value.toString()));
+            context.write(NullWritable.get() , new Text(value.toString()));
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        if (args.length!=4){
-            System.out.println("Usage: MaxTemperature <es.nodes> <es.resource> <es.query> <output path>");
-            System.exit(-1);
-        }
+    public static void main(String[] args) {
+        try {
+            if (args.length != 4) {
+                System.out.println("Usage: MaxTemperature <es.nodes> <es.resource> <es.query> <output path>");
+                System.exit(-1);
+            }
+            long start_time = System.currentTimeMillis();
 
-        long start_time = System.currentTimeMillis();
 
-        Configuration conf = new Configuration();
-        conf.set("es.nodes", args[0]);
-//        conf.set("es.nodes", "106.14.240.250:9200");
-//        conf.set("es.resource", "logstash-2017.11.27/logs");
-        conf.set("es.resource", args[1]);
-        conf.set("es.output.json", "true");
-        conf.set("es.query", "?q="+args[2]);//userid:0f91c56bcf834e4f9e9ad97ee701813d");
+            Configuration conf = new Configuration();
+
+
+
+            conf.set("es.nodes" , args[0]);
+
+            conf.set("es.resource" , args[1]);
+            conf.set("es.output.json" , "true");
+            conf.set("es.query" , "?q=" + args[2]);
+            conf.set("mapred.compress.map.output" , "true");
+            conf.set("mapred.map.output.compression.codec" , "com.hadoop.compression.lzo.LzoCodec");
 
 //        conf.set("io.compression.codecs","org.apache.hadoop.io.compress.GzipCodec, org.apache.hadoop.io.compress.DefaultCodec, com.hadoop.compression.lzo.LzopCodec");
 //        conf.setBoolean("mapred.output.compress",true);
 //        conf.set("mapred.output.compression.codec", "com.hadoop.compression.lzo.LzopCodec");
 
 
-        conf.set("mapred.compress.map.output","true");
-        conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+            Job job = Job.getInstance(conf , "elasticsearch to hadoop");
+//            job.setJarByClass(EH.class);
+
+            // 指定自定义的Mapper阶段的任务处理类
+
+            job.setMapperClass(EhMap.class);
+
+            // 设置输入格式
+            job.setInputFormatClass(EsInputFormat.class);
+
+            // 设置map输出格式
+            job.setMapOutputKeyClass(NullWritable.class);
+            job.setMapOutputValueClass(Text.class);
 
 
-        Job job = Job.getInstance(conf, "elasticsearch to hadoop");
-
-        // 指定自定义的Mapper阶段的任务处理类
-
-        job.setMapperClass(EhMap.class);
-        job.setNumReduceTasks(0);
-        // 设置map输出格式
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-
-        // 设置输入格式
-        job.setInputFormatClass(EsInputFormat.class);
-        // 设置输出路径
-        FileOutputFormat.setOutputPath(job, new Path(args[3]));
+            // 设置输出路径
+            FileOutputFormat.setOutputPath(job , new Path(args[3]));
 //        FileOutputFormat.setOutputPath(job, new Path("hdfs://47.100.76.107:9000/es_output/logstash-new-2017.11.27"));
-        // 运行MR程序
-        job.waitForCompletion(true);
+            // 运行MR程序
+            System.out.println(job.waitForCompletion(true));
 
 
-        // 上面的语句执行完成后，会生成最后的输出文件，需要在此基础上添加lzo的索引
-        // 使用lzo分布式索引生成器
+            // 上面的语句执行完成后，会生成最后的输出文件，需要在此基础上添加lzo的索引
+            // 使用lzo分布式索引生成器
 //        DistributedLzoIndexer lzoIndexer = new DistributedLzoIndexer();
 //        lzoIndexer.setConf(conf);
 //        lzoIndexer.run(new String[]{args[3]});
-//        lzoIndexer.run(new String[]{"hdfs://47.100.76.107:9000/es_output/logstash-new-2017.11.27"});
 
-        System.out.println(System.currentTimeMillis()-start_time);
+
+            System.out.println(System.currentTimeMillis() - start_time);
+
+        } catch (Exception e) {
+            LOG.error(e.getMessage() , e);
+        }
     }
 }
 
--- src/main/java/es/hadoop/HE.java
+++ src/main/java/es/hadoop/HE.java
@@ -1,76 +1,90 @@
 package es.hadoop;
 
-import java.io.IOException;
-
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-
-
 import org.apache.hadoop.mapreduce.Job;
-
-
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
 import org.elasticsearch.hadoop.mr.EsOutputFormat;
-import com.hadoop.mapreduce.LzoTextInputFormat;
+import org.elasticsearch.hadoop.mr.LinkedMapWritable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 
 public class HE {
-
-    public static class HeMapper extends Mapper<Object, Text, NullWritable, Text> {
-        private static final Logger LOG = LoggerFactory.getLogger(HeMapper.class);
-
+    private static Logger LOG = LoggerFactory.getLogger(HE.class);
+    public class HeMapper extends Mapper<LongWritable, Text, NullWritable, LinkedMapWritable> {
         @Override
-        public void map(Object key, Text value, Mapper<Object, Text, NullWritable, Text>.Context context) throws IOException, InterruptedException {
-            byte[] source = value.toString().trim().getBytes();
-            Text jsonDoc = new Text(source);
-            context.write(NullWritable.get(), jsonDoc);
+        protected void map(LongWritable key, Text value, org.apache.hadoop.mapreduce.Mapper.Context context)
+                throws IOException, InterruptedException {
+            LinkedMapWritable doc = new LinkedMapWritable();
+            context.write(NullWritable.get(), doc);
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        if (args.length!=3){
-            System.out.println("Usage: MaxTemperature <es.nodes> <es.resource> <input path>");
-            System.exit(-1);
-        }
-
-        long start_time = System.currentTimeMillis();
-
-        Configuration conf = new Configuration();
-        conf.setBoolean("mapred.map.tasks.speculative.execution", false);
-        conf.setBoolean("mapred.reduce.tasks.speculative.execution", false);
-        conf.set("es.input.json", "yes");
-//        conf.set("es.nodes", args[0]); //106.14.240.250:9200
-//        conf.set("es.resource", args[1]);  //logstash-demo-2017.11.27/logs
-        conf.set("es.nodes", args[0]); //106.14.240.250:9200
-        conf.set("es.resource", args[1]);  //logstash-demo-2017.11.27/logs
-
-        conf.set("mapreduce.map.output.compress", "true");
-        conf.set("mapreduce.map.output.compress.codec", "com.hadoop.compression.lzo.LzoCodec");
 
 
-        Job job = Job.getInstance(conf,"hadoop to elasticsearch");
-        job.setMapperClass(HeMapper.class);
+
+    public static void main(String[] args) {
+        try {
+            if (args.length != 3) {
+                System.out.println("Usage: MaxTemperature <es.nodes> <es.resource> <input path>");
+                System.exit(-1);
+            }
+
+            long start_time = System.currentTimeMillis();
+
+            Configuration conf = new Configuration();
+
+//            String[] oArgs = new GenericOptionsParser(conf , args).getRemainingArgs();
+//
+//
+//
+//
+//
+//            if (oArgs.length != 1) {
+//                LOG.error("error,Usage: MaxTemperature <es.nodes> <es.resource> <input path>\"");
+//                System.exit(2);
+//            }
+
+            conf.setBoolean("mapred.map.tasks.speculative.execution", false);
+            conf.setBoolean("mapred.reduce.tasks.speculative.execution", false);
+            conf.set("es.input.json", "yes");
+
+            conf.set("es.nodes", args[0]);
+            conf.set("es.resource", args[1]);
+
+            conf.setBoolean("mapreduce.map.output.compress", true);
+            conf.set("mapreduce.map.output.compress.codec", "com.hadoop.compression.lzo.LzoCodec");
+
+
+            Job job = Job.getInstance(conf, "hadoop to elasticsearch");
+
+//            job.setJarByClass(HE.class);
+            job.setMapperClass(HeMapper.class);
 
 //        job.setInputFormatClass(LzoTextInputFormat.class);
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(EsOutputFormat.class);
+            job.setInputFormatClass(TextInputFormat.class);
+            job.setOutputFormatClass(EsOutputFormat.class);
 
-        job.setMapOutputKeyClass(NullWritable.class);
-        job.setMapOutputValueClass(Text.class);
+            job.setMapOutputKeyClass(NullWritable.class);
+            job.setMapOutputValueClass(LinkedMapWritable.class);
 
-        // 设置输入路径
-        FileInputFormat.setInputPaths(job, new Path(args[2])); //hdfs://47.100.76.107:9000/es_output/logstash-new-2017.11.27
 
-        job.waitForCompletion(true);
+            // 设置输入路径
+            FileInputFormat.setInputPaths(job, new Path(args[2]));
+            System.out.println(job.waitForCompletion(true));
 
-        System.out.println(System.currentTimeMillis()-start_time);
+            System.out.println(System.currentTimeMillis() - start_time);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
     }
 }
